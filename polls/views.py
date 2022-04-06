41c2a9648884631ac5f0c22c3383187628e76f17
unknown <akaaugustejunior@gmail.com>
2022-04-06 14:14:24 -0400

    correction erreur render with correct render httpResponse

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the polls index.")
