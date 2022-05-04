ea5e473d08c6a51a7de01d1f2f82f1ff64347db5
unknown <akaaugustejunior@gmail.com>
2022-05-03 22:34:30 -0400

    add generic view to avoid copy

--- polls/views.py
+++ polls/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
 # Create your views here.
-def index(request):
-    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': lastest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    def get_queryset(self):
+        """Return the last five published question."""
+        return Question.objects.order_by('-pub_date')[:5]
 
-# done part 3
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/result.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
