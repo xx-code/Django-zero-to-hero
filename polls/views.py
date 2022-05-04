c9fbb5733bf67fafd38e0f3bab87408685cd4e38
unknown <akaaugustejunior@gmail.com>
2022-05-03 21:14:55 -0400

    add a way to display these choice

--- polls/views.py
+++ polls/views.py
@@ -17,9 +17,9 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 # done part 3
-
 def results(request, question_id):
-    return HttpResponse(f"You're looking at the results of question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/result.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
