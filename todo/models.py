4f636b183f52738ed102ffe2f0ef9ea574b4aa39
unknown <akaaugustejunior@gmail.com>
2022-05-06 18:24:52 -0400

    add todo database

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+INDICATOR_TASK = [
+    ('Urgent', 'Urgently '),
+    ('Normal', 'Normal'),
+    ('Low', 'Lower')
+]
+
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=500)
+    due_date = models.DateTimeField('due date')
+    detail = models.TextField()
+    indicator = models.CharField(choices=INDICATOR_TASK, max_length=10)
+
+    def __str__(self):
+        return self.title
+    
+    @property
+    def sort_description(self):
+        raise('Methode not implemented')
+
+
--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import INDICATOR_TASK, Task
 
 # Create your tests here.
+class TaskModelTest(TestCase):
+
+    def test_displays_sort_detail(self):
+        """
+        sort_description() return a short text of detail with three dots at the end
+        """
+        task = Task(title='Do Something Better', 
+        detail='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nulla nunc, semper eu eros dictum, tristique dictum odio. Phasellus lacinia risus at nulla sodales semper. Pellentesque imperdiet, ante vitae ultrices fermentum, sapien est consequat leo, a hendrerit nulla ante at elit. Fusce vel lacus id libero egestas venenatis. Aliquam erat volutpat. Aenean euismod nisi felis, nec imperdiet turpis ultricies non. Mauris arcu metus, vestibulum in est mollis, venenatis fringilla nisl.', 
+        indicator='Normal')
+        
+        self.assertEqual(len(task.sort_description()), 70)
+        self.assertEqual(task.sort_description(), 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nulla...')
+        
