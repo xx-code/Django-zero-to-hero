c53cfd13a5f39bf88c3fe20361f4a8f7a995cc4d
unknown <akaaugustejunior@gmail.com>
2022-05-06 19:40:15 -0400

    creation of view for index

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 
 # Create your views here.
-def index():
+def index(request):
+    return render(request, 'todo/index.html', {})
+
+def add_task():
     raise('Implement this method')
