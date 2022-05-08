5625d1bcbbfd807ea3f0b6ec5583865883b51eff
unknown <akaaugustejunior@gmail.com>
2022-05-08 15:27:14 -0400

    done design add task

--- todo/urls.py
+++ todo/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'todo'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add', views.add_task, name='add_task')
 ]
--- todo/views.py
+++ todo/views.py
@@ -5,5 +5,5 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 def index(request):
     return render(request, 'todo/index.html', {})
 
-def add_task():
-    raise('Implement this method')
+def add_task(request):
+    return render(request, 'todo/add_task.html')
