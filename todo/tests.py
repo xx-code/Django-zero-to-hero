c44863d2bc1483c592fc55af9f29e193642d8c28
unknown <akaaugustejunior@gmail.com>
2022-05-08 15:45:57 -0400

    project fermer car django me fais chier

--- todo/tests.py
+++ todo/tests.py
@@ -1,5 +1,9 @@
+from urllib import response
 from django.test import TestCase
 from .models import INDICATOR_TASK, Task
+
+import datetime
+from django.utils import timezone
 
 # Create your tests here.
 class TaskModelTest(TestCase):
@@ -14,4 +18,15 @@ class TaskModelTest(TestCase):
         
         self.assertEqual(len(task.sort_description), 70)
         self.assertEqual(task.sort_description, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nulla...')
-        
+    
+def creation_task(title, detail, indicator, days):
+    date = timezone.now() - datetime.timedelta(days=days)
+    return Task(title=title, detail=detail, indicator=indicator, due_date=date)
+
+class AddTaskViewTest(TestCase):
+    def test_creation_task_pass_date(self):
+        """
+        no save data when the date of date is in the pass
+        """
+        task = creation_task('Do any task', 'detail detail', 'Normal', -5)
+        response = "done"
